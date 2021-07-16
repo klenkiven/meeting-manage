ecc6bfea3c8fe449cdee5ba02cd806c716487a7e
klenkiven <wzl709@outlook.com>
2021-07-16 14:17:11 +0800

    add: 会议室设备管理接口定义

--- src/main/java/org/tyut4113/meeting/module/sys/controller/MDeviceController.java
+++ src/main/java/org/tyut4113/meeting/module/sys/controller/MDeviceController.java
@@ -1,14 +1,15 @@
 package org.tyut4113.meeting.module.sys.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.AllArgsConstructor;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.tyut4113.meeting.common.utils.Result;
 import org.tyut4113.meeting.module.sys.entity.MDeviceEntity;
+import org.tyut4113.meeting.module.sys.service.MDeviceService;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 设备管理 Controller
@@ -20,12 +21,61 @@ import java.util.List;
 @AllArgsConstructor
 public class MDeviceController {
 
+    private final MDeviceService mDeviceService;
+
     /**
      * 选择设备列表
      */
     @GetMapping("/select")
     @RequiresPermissions("meeting:device:select")
     public Result<List<MDeviceEntity>> select() {
+        return Result.ok();
+    }
+
+    /**
+     * 分页查询设备列表
+     */
+    @GetMapping("/list/{current}/{limit}")
+    @RequiresPermissions("meeting:device:list")
+    public Result<Page<MDeviceEntity>> list(@PathVariable String current,
+                                            @PathVariable String limit,
+                                            @RequestBody Map<String, Object> params) {
+        return Result.ok();
+    }
+
+    /**
+     * 查询设备详细信息
+     */
+    @GetMapping("/info/{deviceId}")
+    @RequiresPermissions("meeting:device:info")
+    public Result<MDeviceEntity> info(@PathVariable String deviceId) {
+        return Result.ok();
+    }
+
+    /**
+     * 保存设备信息
+     */
+    @PostMapping("save")
+    @RequiresPermissions("meeting:device:save")
+    public Result<?> save(@RequestBody MDeviceEntity device) {
+        return Result.ok();
+    }
+
+    /**
+     * 更新设备信息
+     */
+    @PostMapping("update")
+    @RequiresPermissions("meeting:device:update")
+    public Result<?> update(@RequestBody MDeviceEntity device) {
+        return Result.ok();
+    }
+
+    /**
+     * 删除设备信息
+     */
+    @PostMapping("delete")
+    @RequiresPermissions("meeting:device:delete")
+    public Result<?> delete(Long[] ids) {
         return Result.ok();
     }
 
